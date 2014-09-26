131176cf88ec07f3280dc8c62da76357dfc23d1d
npomfret <nick-github@snowmonkey.co.uk>
2014-09-26 14:30:25 +0100

    moved test

--- test/snowmonkey/meeno/EventTypeTest.java
+++ test/snowmonkey/meeno/EventTypeTest.java
@@ -1,18 +1,14 @@
 package snowmonkey.meeno;
 
-import live.AbstractLiveTestCase;
 import live.GenerateTestData;
 import org.junit.Test;
 import snowmonkey.meeno.types.EventTypes;
 
-import static live.GenerateTestData.*;
 import static org.apache.commons.io.FileUtils.*;
 
-public class EventTypeTest extends AbstractLiveTestCase {
+public class EventTypeTest {
     @Test
     public void testRequestForPrices() throws Exception {
-        ukHttpAccess.listEventTypes(fileWriter(GenerateTestData.LIST_EVENT_TYPES_FILE));
-
         EventTypes eventTypes = EventTypes.parse(readFileToString(GenerateTestData.LIST_EVENT_TYPES_FILE.toFile()));
 
         System.out.println(eventTypes.lookup("Soccer").prettyPrint());
