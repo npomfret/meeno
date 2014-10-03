0117e8f3f883bae1ed914acc06eed7f5a45d7a9e
npomfret <nick-github@snowmonkey.co.uk>
2014-10-03 17:00:41 +0100

    cleanup up test a ittle

--- test/live/ListClearedOrdersTest.java
+++ test/live/ListClearedOrdersTest.java
@@ -2,11 +2,11 @@ package live;
 
 import org.junit.Test;
 import snowmonkey.meeno.HttpExchangeOperations;
-import snowmonkey.meeno.types.BetStatus;
 import snowmonkey.meeno.types.ClearedOrderSummary;
 import snowmonkey.meeno.types.ClearedOrderSummaryReport;
 
 import static java.time.ZonedDateTime.*;
+import static snowmonkey.meeno.types.BetStatus.*;
 import static snowmonkey.meeno.types.TimeRange.*;
 
 public class ListClearedOrdersTest extends AbstractLiveTestCase {
@@ -15,9 +15,7 @@ public class ListClearedOrdersTest extends AbstractLiveTestCase {
     public void settledOrders() throws Exception {
         HttpExchangeOperations httpExchangeOperations = ukExchange();
 
-        httpExchangeOperations.listClearedOrders(BetStatus.SETTLED, between(now().minusMonths(3), now()));
-
-        ClearedOrderSummary clearedOrderSummaryReport = httpExchangeOperations.listClearedOrders(BetStatus.SETTLED, between(now().minusMonths(3), now()));
+        ClearedOrderSummary clearedOrderSummaryReport = httpExchangeOperations.listClearedOrders(SETTLED, between(now().minusMonths(3), now()));
 
         for (ClearedOrderSummaryReport orderSummaryReport : clearedOrderSummaryReport.clearedOrders) {
             System.out.println("clearedOrderSummaryReport = " + orderSummaryReport);
@@ -28,7 +26,7 @@ public class ListClearedOrdersTest extends AbstractLiveTestCase {
     public void lapsedOrders() throws Exception {
         HttpExchangeOperations httpExchangeOperations = ukExchange();
 
-        ClearedOrderSummary clearedOrderSummary = httpExchangeOperations.listClearedOrders(BetStatus.LAPSED, between(now().minusMonths(3), now()));
+        ClearedOrderSummary clearedOrderSummary = httpExchangeOperations.listClearedOrders(LAPSED, between(now().minusMonths(3), now()));
 
         for (ClearedOrderSummaryReport orderSummaryReport : clearedOrderSummary.clearedOrders) {
             System.out.println("clearedOrderSummaryReport = " + orderSummaryReport);
