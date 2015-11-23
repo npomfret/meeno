921205e6bf53b3373c4e33d8317e921c3988d96b
npomfret <nick-github@snowmonkey.co.uk>
2015-11-23 12:44:53 +0000

    revert - put fields back into ApiException.java

--- src/main/java/snowmonkey/meeno/ApiException.java
+++ src/main/java/snowmonkey/meeno/ApiException.java
@@ -4,7 +4,14 @@ import org.apache.commons.lang3.StringUtils;
 
 public class ApiException extends Exception {
 
+    public final String errorDetails;
+    public final String errorCode;
+    public final String requestUUID;
+
     public ApiException(String httpStatus, String errorDetails, String errorCode, String requestUUID) {
         super(StringUtils.join(new String[]{httpStatus, errorCode, requestUUID, errorDetails}, ", "));
+        this.errorDetails = errorDetails;
+        this.errorCode = errorCode;
+        this.requestUUID = requestUUID;
     }
 }
